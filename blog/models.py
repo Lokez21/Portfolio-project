2e832386bf83858e87b4006a7178990de61fd337
Lokesh <Lokez21@gmail.com>
2019-08-23 17:08:14 +0100

    jobs complete

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 150)
+    date_posted = models.DateTimeField()
+    image = models.ImageField(upload_to = 'images/')
+    body_text = models.CharField(max_length = 1500)
