5c33013d46ac5cc9f8d6564be909a5d8985ac20f
Lokesh <Lokez21@gmail.com>
2019-09-03 17:18:18 +0100

    Blogs Done.

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from .models import Blog
 # Create your views here.
 
 def allblogs(request):
-    return render(request, 'blog/allblogs.html')
+    blogitems = Blog.objects
+    return render(request, 'blog/allblogs.html', {'blogitems': blogitems})
