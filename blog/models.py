73d8a4e8fe181b7dfe5bf9769502b07f5b01a6b6
Lokesh <Lokez21@gmail.com>
2019-09-13 13:56:58 +0100

    Site complete

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,13 @@ class Blog(models.Model):
     title = models.CharField(max_length = 150)
     date_posted = models.DateTimeField()
     image = models.ImageField(upload_to = 'images/')
-    body_text = models.CharField(max_length = 1500)
+    body_text = models.TextField(max_length = 1500)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body_text[:100]+'(...)'
+
+    def date_posted_pretty(self):
+        return self.date_posted.strftime('%b %e %Y')
