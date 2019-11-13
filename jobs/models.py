9809fab9fe443c87be9b4a6bfffc3e25ca6fc869
Lokesh <lokez21@gmail.com>
2019-11-13 23:54:53 +0000

    added pos, main tweaks

--- jobs/models.py
+++ jobs/models.py
@@ -5,6 +5,6 @@ class Job(models.Model):
     image = models.ImageField(upload_to = 'images/')
     summary = models.CharField(max_length = 250)
     description = RichTextUploadingField(blank=True, null=True)
-
+    position = models.IntegerField(blank=True, null=True)
     def __str__(self):
         return self.summary
